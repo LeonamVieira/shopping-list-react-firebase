63ec26406bd4dd7a87d1ce9d6b7533875c0e31ac
victor <mgvictor7@gmail.com>
2016-12-06 17:55:18 +0000

    Añadida la configuración de Firebase

---
+++ src/config/example.config.js
@@ -0,0 +1,10 @@
+export default Class Config {
+  static function firebaseConfig() {
+    return {
+      apiKey: '',
+      authDomain: '',
+      databaseURL: '',
+      storageBucket: '',
+    };
+  }
+};
